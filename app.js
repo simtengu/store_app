88b6abd616012ebd2657501e4cd2db6d009896d3
Simtengu <albertsimtengu@gmail.com>
2022-12-04 16:31:39 +0300

    nodemon to node....start script fix

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(errorHandlerMiddleware);
 //register not found middleware 
 app.use(notFoundMiddleware);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const start = async ()=>{
   try {
      await connectDb(process.env.MONGO_URI);
